bd61ed62df84dfbf25ac3ce384f69454f07397d4
Nikita Korotaev <websirnik@gmail.com>
2016-12-21 19:28:40 +0000

    Added waitForVendorApi()

--- src/angulartics-piwik.js
+++ src/angulartics-piwik.js
@@ -23,119 +23,122 @@
 
                 $analyticsProvider.settings.pageTracking.trackRelativePath = true;
 
-                // Add piwik specific trackers to angulartics API
+                angulartics.waitForVendorApi('Piwik', 500, function () {
 
-                // Requires the CustomDimensions plugin for Piwik.
-                $analyticsProvider.api.setCustomDimension = function(dimensionId, value) {
+                    // Add piwik specific trackers to angulartics API
 
-                    if ($window._paq) {
-                        $window._paq.push(['setCustomDimension', dimensionId, value]);
-                    }
-                };
+                    // Requires the CustomDimensions plugin for Piwik.
+                    $analyticsProvider.api.setCustomDimension = function(dimensionId, value) {
 
-                // Requires the CustomDimensions plugin for Piwik.
-                $analyticsProvider.api.deleteCustomDimension = function(dimensionId) {
-
-                    if ($window._paq) {
-                        $window._paq.push(['deleteCustomDimension', dimensionId]);
-                    }
-                };
-
-                // scope: visit or page. Defaults to 'page'
-                $analyticsProvider.api.setCustomVariable = function(varIndex, varName, value, scope) {
-
-                    if ($window._paq) {
-                        scope = scope || 'page';
-                        $window._paq.push(['setCustomVariable', varIndex, varName, value, scope]);
-                    }
-                };
-
-                // scope: visit or page. Defaults to 'page'
-                $analyticsProvider.api.deleteCustomVariable = function(varIndex, scope) {
-
-                    if ($window._paq) {
-                        scope = scope || 'page';
-                        $window._paq.push(['deleteCustomVariable', varIndex, scope]);
-                    }
-                };
-
-                // trackSiteSearch(keyword, category, [searchCount])
-                $analyticsProvider.api.trackSiteSearch = function(keyword, category, searchCount) {
-
-                    // keyword is required
-                    if ($window._paq && keyword) {
-
-                        var params = ['trackSiteSearch', keyword, category || false];
-
-                        // searchCount is optional
-                        if (angular.isDefined(searchCount)) {
-                            params.push(searchCount);
+                        if ($window._paq) {
+                            $window._paq.push(['setCustomDimension', dimensionId, value]);
                         }
+                    };
 
-                        $window._paq.push(params);
-                    }
-                };
+                    // Requires the CustomDimensions plugin for Piwik.
+                    $analyticsProvider.api.deleteCustomDimension = function(dimensionId) {
 
-                // logs a conversion for goal 1. revenue is optional
-                // trackGoal(goalID, [revenue]);
-                $analyticsProvider.api.trackGoal = function(goalID, revenue) {
-                    if ($window._paq) {
-                        _paq.push(['trackGoal', goalID, revenue || 0]);
-                    }
-                };
-
-                // track outlink or download
-                // linkType is 'link' or 'download', 'link' by default
-                // trackLink(url, [linkType]);
-                $analyticsProvider.api.trackLink = function(url, linkType) {
-                    var type = linkType || 'link';
-                    if ($window._paq) {
-                        $window._paq.push(['trackLink', url, type]);
-                    }
-                };
-
-                // Set default angulartics page and event tracking
-
-                // $analytics.setUsername(username)
-                $analyticsProvider.registerSetUsername(function(username) {
-                    if ($window._paq) {
-                        $window._paq.push(['setUserId', username]);
-                    }
-                });
-
-                // $analytics.setAlias(alias)
-                // $analyticsProvider.registerSetAlias(function(param) {
-                //     // TODO: No piwik corresponding function found. Use setCustomVariable instead
-                // });
-
-                // $analytics.setUserProperties(properties)
-                // $analyticsProvider.registerSetUserProperties(function(param) {
-                //     // TODO: No piwik corresponding function found. Use setCustomVariable instead
-                // });
-
-                // locationObj is the angular $location object
-                $analyticsProvider.registerPageTrack(function(path, locationObj) {
-
-                    if ($window._paq) {
-                        $window._paq.push(['setDocumentTitle', $window.document.title]);
-                        $window._paq.push(['setCustomUrl', path]);
-                        $window._paq.push(['trackPageView']);
-                    }
-                });
-
-                // trackEvent(category, event, [name], [value])
-                $analyticsProvider.registerEventTrack(function(action, properties) {
-
-                    if ($window._paq) {
-
-                        // PAQ requires that eventValue be an integer, see: http://piwik.org/docs/event-tracking/
-                        if (properties.value) {
-                            var parsed = parseInt(properties.value, 10);
-                            properties.value = isNaN(parsed) ? 0 : parsed;
+                        if ($window._paq) {
+                            $window._paq.push(['deleteCustomDimension', dimensionId]);
                         }
+                    };
 
-                        $window._paq.push(['trackEvent', properties.category, action, properties.label, properties.value]);
-                    }
+                    // scope: visit or page. Defaults to 'page'
+                    $analyticsProvider.api.setCustomVariable = function(varIndex, varName, value, scope) {
+
+                        if ($window._paq) {
+                            scope = scope || 'page';
+                            $window._paq.push(['setCustomVariable', varIndex, varName, value, scope]);
+                        }
+                    };
+
+                    // scope: visit or page. Defaults to 'page'
+                    $analyticsProvider.api.deleteCustomVariable = function(varIndex, scope) {
+
+                        if ($window._paq) {
+                            scope = scope || 'page';
+                            $window._paq.push(['deleteCustomVariable', varIndex, scope]);
+                        }
+                    };
+
+                    // trackSiteSearch(keyword, category, [searchCount])
+                    $analyticsProvider.api.trackSiteSearch = function(keyword, category, searchCount) {
+
+                        // keyword is required
+                        if ($window._paq && keyword) {
+
+                            var params = ['trackSiteSearch', keyword, category || false];
+
+                            // searchCount is optional
+                            if (angular.isDefined(searchCount)) {
+                                params.push(searchCount);
+                            }
+
+                            $window._paq.push(params);
+                        }
+                    };
+
+                    // logs a conversion for goal 1. revenue is optional
+                    // trackGoal(goalID, [revenue]);
+                    $analyticsProvider.api.trackGoal = function(goalID, revenue) {
+                        if ($window._paq) {
+                            _paq.push(['trackGoal', goalID, revenue || 0]);
+                        }
+                    };
+
+                    // track outlink or download
+                    // linkType is 'link' or 'download', 'link' by default
+                    // trackLink(url, [linkType]);
+                    $analyticsProvider.api.trackLink = function(url, linkType) {
+                        var type = linkType || 'link';
+                        if ($window._paq) {
+                            $window._paq.push(['trackLink', url, type]);
+                        }
+                    };
+
+                    // Set default angulartics page and event tracking
+
+                    // $analytics.setUsername(username)
+                    $analyticsProvider.registerSetUsername(function(username) {
+                        if ($window._paq) {
+                            $window._paq.push(['setUserId', username]);
+                        }
+                    });
+
+                    // $analytics.setAlias(alias)
+                    // $analyticsProvider.registerSetAlias(function(param) {
+                    //     // TODO: No piwik corresponding function found. Use setCustomVariable instead
+                    // });
+
+                    // $analytics.setUserProperties(properties)
+                    // $analyticsProvider.registerSetUserProperties(function(param) {
+                    //     // TODO: No piwik corresponding function found. Use setCustomVariable instead
+                    // });
+
+                    // locationObj is the angular $location object
+                    $analyticsProvider.registerPageTrack(function(path, locationObj) {
+
+                        if ($window._paq) {
+                            $window._paq.push(['setDocumentTitle', $window.document.title]);
+                            $window._paq.push(['setCustomUrl', path]);
+                            $window._paq.push(['trackPageView']);
+                        }
+                    });
+
+                    // trackEvent(category, event, [name], [value])
+                    $analyticsProvider.registerEventTrack(function(action, properties) {
+
+                        if ($window._paq) {
+
+                            // PAQ requires that eventValue be an integer, see: http://piwik.org/docs/event-tracking/
+                            if (properties.value) {
+                                var parsed = parseInt(properties.value, 10);
+                                properties.value = isNaN(parsed) ? 0 : parsed;
+                            }
+
+                            $window._paq.push(['trackEvent', properties.category, action, properties.label, properties.value]);
+                        }
+                    });
                 });
             }
         ]);
